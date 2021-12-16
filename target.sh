ab62561f72b05a9ef94059aab7e0c3216f1e5c69
fti-vsaxena <94839845+fti-vsaxena@users.noreply.github.com>
2021-12-16 19:33:02 +0530

    Update target.sh

--- target.sh
+++ target.sh
@@ -4,4 +4,4 @@
 
 #v4l2src device=/dev/video0 ! videoconvert! videoscale ! video/x-raw, width=640, height=480 ! autovideosink -v
 
-! gst-launch-1.0 v4l2src device=/dev/video0 ! video/x-raw , framerate=30/1 ! jpegenc ! zmqsink
+! gst-launch-1.0 -v v4l2src device=/dev/video0 ! videoconvert ! videoscale ! video/x-raw , framerate=30/1 ! zmqsink
