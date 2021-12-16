3bde159cc0f8369176480ed6ae685269e92c4856
fti-vsaxena <94839845+fti-vsaxena@users.noreply.github.com>
2021-12-16 19:31:20 +0530

    Update recieve.sh

--- recieve.sh
+++ recieve.sh
@@ -1,5 +1,4 @@
-#! gst-launch-1.0 zmqsrc endpoint="tcp://172.18.0.2:5556" ! videoconvert ! videoscale ! video/x-raw , width=640, height=480 , framerate=10/1 ! multifilesink location=snapshot-%05d.jpg -v
+! gst-launch-1.0 zmqsrc endpoint="tcp://172.18.0.2:5556" ! fdsink fd=1
 
-#! gst-launch-1.0 zmqsrc endpoint="tcp://172.18.0.2:5556" ! fdsink fd=1
+! gst-launch-1.0 -v v4l2src device=/dev/video0 ! videoconvert ! videoscale ! video/x-raw , framerate=30/1 ! zmqsink
 
-! gst-launch-1.0 zmqsrc endpoint="tcp://172.18.0.2:5556" ! video/x-raw, width=640, height=480, framerate=30/1 ! jpegenc ! multifilesink location=snapshot-%05d.jpg
